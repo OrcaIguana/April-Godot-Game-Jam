d35286649fdfcde36cf762dee310e99b681e7649
Josh Aird <airdjosh37@gmail.com>
2025-04-13 00:22:53 -0600

    Working on player bullets

--- Scripts/active_wand.gd
+++ Scripts/active_wand.gd
@@ -2,11 +2,19 @@ extends Node2D
 
 var wand_images = [preload("res://icon.svg"), preload("res://Sprites/circle.png"), preload("res://Sprites/Slot.png")]
 
+var bullets = preload("res://player_bullet.tscn")
+
+func _process(delta):
+	if Input.is_action_just_released("shoot"):
+		shoot()
+
 func _physics_process(delta):
 	var mouse_pos = get_viewport().get_mouse_position()
 	self.position = Vector2(0, 0)
 	self.position = self.global_position.direction_to(mouse_pos) * 100
 
-func _on_button_on_press(id):
-	print("Signal Recieved")
-	get_node("Sprite2D2").change_wand_image()
+func shoot():
+	var instance = bullets.instantiate()
+	get_tree().current_scene.add_child(instance)
+	print("Spawned Bullet")
+	

8b3b0b20570a9e0cf3df80e12a1900c9ccd694af
Josh Aird <airdjosh37@gmail.com>
2025-04-13 14:17:15 -0600

    Added modular wand system

--- Scripts/active_wand.gd
+++ Scripts/active_wand.gd
@@ -2,22 +2,51 @@ extends Node2D
 
 const bullets = preload("res://Player/player_bullet.tscn")
 
-var cooldown = 5
+var max_cooldown = 0.5
 var time_to_kill = 0.5
 var projectiles = 1
+var burst = 100
+var burst_speed = 0.1
+var spread = 10
+
+var cooldown = 0
 
 func _process(delta):
 	if Input.is_action_just_released("shoot"):
-		var instance = shoot(self.global_position, time_to_kill, projectiles)
-		get_tree().current_scene.add_child(instance)
+		if cooldown <= 0:
+			shoot(time_to_kill, projectiles, burst, spread)
+			cooldown = max_cooldown
+	cooldown -= delta
 
 func _physics_process(delta):
 	var mouse_pos = get_viewport().get_mouse_position()
 	self.position = Vector2(0, 0)
 	self.position = self.global_position.direction_to(mouse_pos) * 500
 
-static func shoot(global_position: Vector2, time_to_kill, amount_of_projectiles):
-	var instance = bullets.instantiate()
-	instance.get_node("CharacterBody2D").spawn_position = global_position
-	print("Spawned Bullet")
-	return instance
+func shoot(time_to_kill, amount_of_projectiles, burst, spread):
+	var counter = 0
+	var loop = 0
+	var instances = []
+	var negative = false
+	while true:
+		var mouse_pos = get_viewport().get_mouse_position()
+		var direction = global_position.direction_to(mouse_pos)
+		for projectiles in range(spread):
+			var instance = bullets.instantiate()
+			var bullet = instance.get_node("CharacterBody2D")
+			if negative:
+				bullet.direction = direction.rotated(deg_to_rad(-5*counter))
+				negative = false
+			else:
+				bullet.direction = direction.rotated(deg_to_rad(5*counter))
+				negative = true
+				counter += 1
+			bullet.spawn_position = self.global_position
+			instances.append(instance)
+		for objects in instances:
+			get_tree().current_scene.add_child(objects)
+		loop += 1
+		if loop >= burst:
+			break
+		await get_tree().create_timer(burst_speed).timeout
+		counter = 0

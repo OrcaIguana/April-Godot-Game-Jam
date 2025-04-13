2a8ad1c885f16a16a1256cd3fb87cfecbb748862
OrcaIguana <lpettit0519@hotmail.com>
2025-04-13 16:14:57 -0600

    Floor Print

--- Tile Sets/tile_map_layer.gd
+++ Tile Sets/tile_map_layer.gd
@@ -1,9 +1,20 @@
 extends TileMapLayer
 
-func _ready() -> void:
-	_generate_room(Vector2i(0,0))
+var _room_cords = Vector2i(0,0)
+var _room_width = 28
+var _room_height = 16
 
-func _generate_room(_room_cords) -> void:
-	var placement_x = 0
-	var placement_y = 0
-	set_cell(Vector2i(_room_cords.x+placement_x, _room_cords.y+placement_y), 0 , Vector2i(1,0))
+func _ready() -> void:
+	_initialize_room()
+
+func _make_dungeon():
+	pass
+
+func _initialize_room():
+	var _tile_cords = Vector2i(0,0)
+	for i in range(_room_width):
+		for j in range(0,_room_height):
+			set_cell(Vector2i(_room_cords.x*28+_tile_cords.x,_room_cords.y*16+_tile_cords.y),0,Vector2i(randi_range(0,2),0))
+			_tile_cords += Vector2i(0,1)
+		_tile_cords += Vector2i(1,0)
+		_tile_cords -= Vector2i(0,_room_height)

2ec08b997594a98c3f0f80606110e2c84d7e26b3
OrcaIguana <lpettit0519@hotmail.com>
2025-04-13 16:33:21 -0600

    Fixed Floor generation

--- Tile Sets/tile_map_layer.gd
+++ Tile Sets/tile_map_layer.gd
@@ -1,8 +1,8 @@
 extends TileMapLayer
 
 var _room_cords = Vector2i(0,0)
-var _room_width = 28
-var _room_height = 16
+var _room_width = 26
+var _room_height = 14
 
 func _ready() -> void:
 	_initialize_room()
@@ -11,10 +11,12 @@ func _make_dungeon():
 	pass
 
 func _initialize_room():
-	var _tile_cords = Vector2i(0,0)
+	var _tile_cords = Vector2i(1,1)
 	for i in range(_room_width):
-		for j in range(0,_room_height):
+		for j in range(_room_height):
 			set_cell(Vector2i(_room_cords.x*28+_tile_cords.x,_room_cords.y*16+_tile_cords.y),0,Vector2i(randi_range(0,2),0))
 			_tile_cords += Vector2i(0,1)
 		_tile_cords += Vector2i(1,0)
 		_tile_cords -= Vector2i(0,_room_height)
+	
+	

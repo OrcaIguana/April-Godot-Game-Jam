17dea0f4db9a16d8afb02d2267bb08a4b6074b81
Strobesque <strobescent@gmail.com>
2025-04-16 18:43:51 -0800

    Fixed XP on my end

--- Enemies/xp.gd
+++ Enemies/xp.gd
@@ -2,8 +2,13 @@ extends Node2D
 
 var spawn_position = Vector2()
 
+var type = "XP"
+
 func _ready():
 	self.position = spawn_position
 
 func _on_area_2d_kill() -> void:
+	queue_free()
+
+func kill_self():
 	queue_free()
